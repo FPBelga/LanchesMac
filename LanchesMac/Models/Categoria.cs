cd5cb5cc5c382d5252878491e80b68bd9a45b94e
FPBelga <felipebd25@hotmail.com>
2024-05-20 19:45:13 -0300

    Criando o Modelo de dominio

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaÍd { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set; }
+    }
+
+}
