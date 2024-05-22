364c8415255013879c360c85a1cd8e8e320388ab
FPBelga <belgasystem@gmail.com>
2024-05-22 08:22:31 -0300

    Populando a tabela Lanches com dados iniciais

---
+++ LanchesMac/Migrations/20240522105556_PopularLanches.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularLanches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) " +
+                "                 VALUES(1,'Pão, Hamburger, ovo, presunto, queijo e batata palha','Delicioso pão de hamburger com queijo de primeira qualidade acompanhado com batata Palha',1,'http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg','http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg',0,'Cheese Salada', 12.50)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) " +
+                "                 VALUES(1,'Pão, presunto, musarela e tomate','Delicioso pão francês quentinho e queijo de primeira qualidade ',1,'http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg','http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg',0,'Misto quente', 8.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) " +
+                "                 VALUES(1,'Pão, Hamburger, presunto, queijo e batata palha','Delicioso pão de hamburger com queijo de primeira qualidade acompanhado com batata Palha',1,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg',0,'Cheese Burger', 11.00)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Nome, Preco) " +
+                "                 VALUES(2,'Pão Integral, queijo branco, peito de peru, cenoura, alface, iogurte','Delicioso pão Integral com queijo de bufala de qualidade acompanhado com iogurte',1,'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',1,'Lanche Natural Peito de Peru ', 15.00)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
