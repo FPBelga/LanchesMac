b9f51e2fb3b90a9a0806ed84c7c94d92fd6b6963
FPBelga <felipebd25@hotmail.com>
2024-07-17 13:11:29 -0300

    108. Implementando a configuração na classe Program e removendo a classe Startup

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,15 +1,158 @@
-namespace LanchesMac;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+//namespace LanchesMac;
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        CreateHostBuilder(args).Build().Run();
+//    }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+//    public static IHostBuilder CreateHostBuilder(string[] args) =>
+//        Host.CreateDefaultBuilder(args)
+//            .ConfigureWebHostDefaults(webBuilder =>
+//            {
+//                webBuilder.UseStartup<Startup>();
+//            });
+//}
+
+using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Areas.Services;
+using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//Registrando os serviços do Identity para Controle de Usuário
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+     .AddEntityFrameworkStores<AppDbContext>()
+     .AddDefaultTokenProviders();
+
+// builder.Services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "Home/AccesDenied");
+builder.Services.Configure<ConfigurationImagens>(builder.Configuration.GetSection("ConfigurationPastaImagens"));
+
+//builder.Services.Configure<IdentityOptions>(options =>
+//{
+//    // Default Password settings.
+//    options.Password.RequireDigit = false;
+//    options.Password.RequireLowercase = false;
+//    options.Password.RequireNonAlphanumeric = false;
+//    options.Password.RequireUppercase = false;
+//    options.Password.RequiredLength = 3;
+//    options.Password.RequiredUniqueChars = 1;
+//});
+
+//Criando a injenção de dependência no container nativo para criar a instância do objeto da interface
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+builder.Services.AddScoped<RelatorioVendasService>();
+builder.Services.AddScoped<GraficoVendasService>();
+
+//Incluindo a Politica de acesso informando o perfil "Admin" como necessário
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin",
+        politica =>
+        {
+            politica.RequireRole("Admin");
+        });
+});
+
+//Habilitando os recursos do HTTPContext
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+//Cria o carrinho a cada requisição. Cria um objeto scoped, ou seja um objeto que esta associado a requisição
+//Isso significa que se  duas pessoas solicitarem o objeto CarrinhoCompra ao mesmo tempo
+//elas vão obter instâncias diferentes
+builder.Services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+//Habilitando o cache da sessão para otimizar a sessão
+builder.Services.AddMemoryCache();
+//builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession();
+//{
+//    options.IdleTimeout = TimeSpan.FromSeconds(10);
+//    options.Cookie.HttpOnly = true;
+//    options.Cookie.IsEssential = true;
+//});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+CriarPerfisUsuarios(app);
+
+////Cria os usuários e atribui ao perfil
+//seedUserRoleInitial.SeedRoles();
+////cria os usuários e atribui ao perfil
+//seedUserRoleInitial.SeedUsers();
+
+//Ativando o middleware da sessão
+app.UseSession();
+app.UseSession();
+
+//Ativando o middleware de autenticação
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+     name: "areas",
+     pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+       name: "categoriaFiltro",
+       pattern: "Lanche/{action}/{categoria?}",
+       defaults: new { Controller = "Lanche", action = "List" });
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+
+void CriarPerfisUsuarios(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedUsers();
+        service.SeedRoles();
+    }
 }
